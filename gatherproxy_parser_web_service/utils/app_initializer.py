bf8f86c39a07f7fc6791d5f971a9216bfc2ce340
Ivanov Eugene <erqups@gmail.com>
2019-08-26 15:10:30 +0300

    feat: update init_app method

--- gatherproxy_parser_web_service/utils/app_initializer.py
+++ gatherproxy_parser_web_service/utils/app_initializer.py
@@ -1,11 +1,13 @@
 import yaml
-from os import path
+import pathlib
+from os import path, makedirs
 from aiohttp import web
-from ..common.exceptions import (ConfigFileNotExists, ConfigFileParsingException, ConfigValidationException)
-from ..utils.logging import Logging
+from ..utils.exceptions import (ConfigFileNotExistsException, ConfigFileParsingException,
+                                ConfigValidationException)
+from ..common.logging import Logging
 from .schemas import ConfigSchema
 from ..api.routes import RoutesInstaller
-from ..api.views import CollectRequestHandler
+from ..api.views import CollectV1Handler
 
 
 class AppInitializer:
@@ -17,7 +19,7 @@ class AppInitializer:
     def _get_config_from_file(self) -> dict:
         if not path.exists(self._config_file):
             self._logger.error(f'File "{self._config_file}" not exists')
-            raise ConfigFileNotExists(f'File "{self._config_file}" not exists')
+            raise ConfigFileNotExistsException(f'File "{self._config_file}" not exists')
 
         try:
             with open(file=self._config_file, mode="r") as file:
@@ -43,6 +45,13 @@ class AppInitializer:
 
         return self._config
 
+    def _create_log_file_path(self, log_file_path: str) -> None:
+        log_file_path = pathlib.Path(log_file_path).resolve()
+        if not path.exists(log_file_path.parent):
+            makedirs(log_file_path.parent)
+
     def init_app(self, app: web.Application) -> None:
-        RoutesInstaller(app=app, handler=CollectRequestHandler()).install()
-        print(self.config)
+        self._create_log_file_path(log_file_path=self.config["app_settings"]["log_file_path"])
+        Logging.init_root_logger(level=self.config["app_settings"]["log_level"],
+                                 log_file_path=self.config["app_settings"]["log_file_path"])
+        RoutesInstaller(app=app, handler=CollectV1Handler(app_config=self.config)).install()
