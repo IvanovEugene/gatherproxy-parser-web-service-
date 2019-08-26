c1267357336d5500e9d5af1a47c61c831797ba39
Ivanov Eugene <erqups@gmail.com>
2019-08-26 15:08:42 +0300

    feat: update app initializing

--- gatherproxy_parser_web_service/api/app.py
+++ gatherproxy_parser_web_service/api/app.py
@@ -1,17 +1,14 @@
-import asyncio
 from aiohttp import web
 from ..utils.app_initializer import AppInitializer
 from ..common.constants import PROJECT_ROOT
 
-def main() -> None:
-    # loop = asyncio.get_event_loop()
-    app = web.Application()
-    config_file = PROJECT_ROOT / 'config' / 'config.yaml'
-    AppInitializer(config_file=config_file).init_app(app)
 
-    """
-    loop = asyncio.get_event_loop()
-    app = loop.run_until_complete(init_app(conf))
-    web.run_app(app, host=conf.app.host, port=conf.app.port)
-    """
-    web.run_app(app)
+app = web.Application()
+config_file = PROJECT_ROOT / 'config' / 'config.yaml'
+app_initializer = AppInitializer(config_file=config_file)
+app_initializer.init_app(app)
+
+app_settings = app_initializer.config["app_settings"]
+host, port = app_settings["host"], app_settings["port"]
+
+web.run_app(app, host=host, port=port)
