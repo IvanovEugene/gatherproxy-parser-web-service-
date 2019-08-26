537d4c63a50eee457a79f5139cfe7b7d962d7f9f
Ivanov Eugene <erqups@gmail.com>
2019-08-26 15:09:44 +0300

    feat: remove log-level, add init_root_logger method

--- gatherproxy_parser_web_service/common/logging.py
+++ gatherproxy_parser_web_service/common/logging.py
@@ -1,23 +1,38 @@
 # encoding: utf-8
 # TODO: migrate to aiologger
+import os
+import pathlib
 import sys
 import logging
 
 
 class Logging:
     @staticmethod
-    def get_logger(name: str, level=logging.DEBUG) -> logging.Logger:
-        logger = logging.getLogger(name)
-        logger.setLevel(level)
+    def init_root_logger(level=logging.DEBUG, log_file_path: str = None) -> None:
+        root_logger = logging.root
+        root_logger.handlers.clear()
+        root_logger.setLevel(level)
+        # Setting that handlers only for root logger because downstream loggers propagate messages to root logger
         # Stdout handler
         stdout_handler = logging.StreamHandler(sys.stdout)
         stdout_handler.addFilter(lambda entry: entry.levelno <= logging.INFO)
-        stdout_handler.setFormatter(logging.Formatter('[%(levelname)s] [%(asctime)s] %(module)s [%(name)s] %(message)s'))
-        logger.addHandler(stdout_handler)
+        stdout_handler.setFormatter(logging.Formatter('[%(levelname)s]'
+                                                      ' [%(asctime)s] %(module)s [%(name)s] %(message)s'))
+        root_logger.addHandler(stdout_handler)
         # Stderr handler
         stderr_handler = logging.StreamHandler(sys.stderr)
         stderr_handler.addFilter(lambda entry: entry.levelno > logging.INFO)
-        stderr_handler.setFormatter(logging.Formatter('[%(levelname)s] [%(asctime)s] %(module)s [%(name)s] %(message)s'))
-        logger.addHandler(stderr_handler)
+        stderr_handler.setFormatter(logging.Formatter('[%(levelname)s]'
+                                                      ' [%(asctime)s] %(module)s [%(name)s] %(message)s'))
+        root_logger.addHandler(stderr_handler)
+        # File handler
+        if log_file_path:
+            file_handler = logging.FileHandler(filename=log_file_path)
+            root_logger.addHandler(file_handler)
+
+    @staticmethod
+    def get_logger(name: str) -> logging.Logger:
+        logger = logging.getLogger(name)
+        logger.propagate = True
 
         return logger
